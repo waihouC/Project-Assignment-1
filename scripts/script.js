31bc25f8e7d17680716e620104b1e19591a34c41
♠C♥W♦H♣ ‎‎ <changwh5@gmail.com>
2021-08-07 00:53:20 +0800

    check in: - added map clustering - added new hawker info - added geosearch function

--- scripts/script.js
+++ scripts/script.js
@@ -19,10 +19,28 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
 }).addTo(map);
 
-// load hawker markers into map
-async function getHawkerLayer() {
+// create marker cluster layer
+let markerClusterLayer = L.markerClusterGroup();
+
+const openedStatus = "<span style='color: green'>Available</span>"
+const closedStatus = "<span style='color: red'>Closed</span>"
+
+function DateWithinInterval(startDate, endDate) {
+    // check inputs are valid dates
+    var isStartDateValid = moment(startDate, "D/M/YYYY").isValid();
+    var isEndDateValid = moment(endDate, "D/M/YYYY").isValid();
+    if (!isStartDateValid || !isEndDateValid) {
+        return false;
+    }
+    
+    const now = moment().format("D/M/YYYY");
+    return moment(now).isBetween(startDate, endDate, undefined, "[]");
+}
+
+// load hawker markers into cluster layer
+async function getHawkerLayer(csvData) {
     let response = await axios.get("data/hawker-centres.geojson");
-    let hawkerLayer = L.geoJson(response.data, {
+    L.geoJson(response.data, {
         filter: function (feature, layer) {
             // filter away 'under construction' status
             return (!feature.properties.Description.toLowerCase().includes("under construction"));
@@ -32,10 +50,54 @@ async function getHawkerLayer() {
             e.innerHTML = feature.properties.Description;
             let tds = e.querySelectorAll('td');
 
-            // extract info
+            // extract geojson info
             let photourl = tds[17].innerHTML;
             let name = tds[19].innerHTML;
             let address = tds[29].innerHTML;
+
+            // extract csv info
+            let hawkerData = csvData.filter(function(row) {
+                return row.name == name;
+            }).map(function(row) {
+                return {
+                    'name': row.name,
+                    'description': row.description_myenv,
+                    'foodstalls': row.no_of_food_stalls,
+                    'q1_cleaningstartdate': row.q1_cleaningstartdate,
+                    'q1_cleaningenddate': row.q1_cleaningenddate,
+                    'q2_cleaningstartdate': row.q2_cleaningstartdate,
+                    'q2_cleaningenddate': row.q2_cleaningenddate,
+                    'q3_cleaningstartdate': row.q3_cleaningstartdate,
+                    'q3_cleaningenddate': row.q3_cleaningenddate,
+                    'q4_cleaningstartdate': row.q4_cleaningstartdate,
+                    'q4_cleaningenddate': row.q4_cleaningenddate,
+                    'others_startdate': row.other_works_startdate,
+                    'others_enddate': row.other_works_enddate
+                }
+            });
+            // logging
+            if (hawkerData == "" || hawkerData == undefined) {
+                console.log('No record for ' + name);
+            }
+
+            // take care of records with no data
+            let description = (hawkerData == "" || hawkerData == undefined) ? "" : hawkerData[0].description;
+            let foodstalls = (hawkerData == "" || hawkerData == undefined) ? "" : hawkerData[0].foodstalls;
+            // create a <span> tag for availability
+            let status;
+            if (hawkerData == "" || hawkerData == undefined) {
+                status = openedStatus;
+            } else {
+                if (DateWithinInterval(hawkerData[0].q1_cleaningstartdate, hawkerData[0].q1_cleaningenddate) ||
+                    DateWithinInterval(hawkerData[0].q2_cleaningstartdate, hawkerData[0].q2_cleaningenddate) ||
+                    DateWithinInterval(hawkerData[0].q3_cleaningstartdate, hawkerData[0].q3_cleaningenddate) ||
+                    DateWithinInterval(hawkerData[0].q4_cleaningstartdate, hawkerData[0].q4_cleaningenddate) ||
+                    DateWithinInterval(hawkerData[0].others_startdate, hawkerData[0].others_enddate)) {
+                        status = closedStatus;
+                    } else {
+                        status = openedStatus;
+                    }
+            }
 
             // some names contain address, need to filter away
             let regEx = /\(.+\)/;
@@ -47,21 +109,45 @@ async function getHawkerLayer() {
                 name = name.slice(0, -1);
             }
 
-            layer.bindPopup(`<table>
+            layer.bindPopup(`<table class="table table-striped">
                 <tr>
-                    <th style="padding:5px">
+                    <th>
                         Name:
                     </th>
-                    <td style="padding:5px">
+                    <td>
                         ${name}
                     </td>
                 </tr>
                 <tr>
-                    <th style="padding:5px">
+                    <th>
                         Address:
                     </th>
-                    <td style="padding:5px">
+                    <td>
                         ${address}
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Description:
+                    </th>
+                    <td>
+                        ${description}
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Number of food stalls:
+                    </th>
+                    <td>
+                        ${foodstalls}
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        Availability:
+                    </th>
+                    <td>
+                        ${status}
                     </td>
                 </tr>
                 <tr>
@@ -71,11 +157,24 @@ async function getHawkerLayer() {
                 </tr>
             </table>`);
         }
-    }).addTo(map);
+    }).addTo(markerClusterLayer);
 };
 
-window.addEventListener('DOMContentLoaded', async () => {
-    await getHawkerLayer();
+// load CSV file data and combine with geojson
+function loadHawkerData() {
+    // papaparse is used to parse csv file into json string
+    Papa.parse('data/dates-of-hawker-centres-closure.csv', {
+        download: true,
+        header: true,
+        complete: async function(results) {
+            await getHawkerLayer(results.data);
+        }
+    });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    loadHawkerData();
+    markerClusterLayer.addTo(map);
 });
 
 // popovers at footer links
@@ -118,3 +217,37 @@ $(function () {
     });
 })
 
+// Geocoder search
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+const geocoder = L.Control.geocoder({
+    // fix location to SG
+    geocoder: L.Control.Geocoder.nominatim({
+        geocodingQueryParams: {countrycodes: 'SG'}
+    }),
+    defaultMarkGeocode: false,
+    placeholder: 'Where would you like to go?',
+    collapsed: false
+}).addTo(map);
+
+let searchLocation;
+geocoder.on('markgeocode', function(e) {
+    // remove previous searches
+    if (searchLocation) {
+        map.removeLayer(searchLocation);
+    }
+
+    const latlng = e.geocode.center;
+    searchLocation = L.marker(latlng).addTo(map);
+    map.fitBounds(e.geocode.bbox);
+
+    // clear text input
+    geocoder.setQuery("");
+}).addTo(map);
+
+// pull the geocoder search out of map
+document.getElementById('search-box').appendChild(
+    document.querySelector(".leaflet-control-geocoder")
+);
